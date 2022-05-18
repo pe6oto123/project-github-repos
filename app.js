56063b14cda71a03f178558be3780303d1a65145
pe6oto123 <pe6oto123@gmail.com>
2022-05-18 20:04:03 +0300

    finished the project

--- app.js
+++ app.js
@@ -1,26 +1,11 @@
 #!/usr/bin/evn node
 import axios from 'axios'
 import inquirer from 'inquirer'
-import fs from 'fs'
-import request from 'superagent'
+import fetch from 'node-fetch'
+import fs from 'fs-extra'
+import superagent from 'superagent'
 import admZip from 'adm-zip'
 import { createSpinner } from 'nanospinner'
-
-if (!fs.existsSync('./repositories')) {
-    fs.mkdirSync('./repositories')
-}
-
-var repositories = {}
-
-var searchParam = {
-    repo: null,
-    lang: null,
-    num: null
-}
-
-var downloads = []
-
-console.log('\n\n\n')
 
 const inputPrams = async () => {
     const input = await inquirer.prompt([
@@ -50,13 +35,15 @@ const inputPrams = async () => {
         }
     ])
 
-    searchParam.repo = input.repoInput
-    searchParam.lang = input.langInput
-    searchParam.num = input.numInput
+    return {
+        repo: input.repoInput,
+        lang: input.langInput,
+        num: input.numInput
+    }
 }
 
 
-const getRepos = async () => {
+const getRepos = async (searchParam) => {
     try {
         var req = await axios(`https://api.github.com/search/repositories?q=${searchParam.repo}+language:${searchParam.lang}&per_page=${searchParam.num}`)
     } catch (error) {
@@ -66,22 +53,52 @@ const getRepos = async () => {
     return req.data.items
 }
 
-await inputPrams()
-repositories = await getRepos()
+const handleDownloads = async (repo, dir_downloadFolder, index) => {
 
-for (var i = 0; i < Array.from(repositories).length; ++i) {
-    var obj = {
-        name: repositories[i].name,
-        description: repositories[i].description,
-        html_url: repositories[i].html_url + '/archive/master.zip'
-    }
 
-    downloads.push(obj)
+    const downloadMessage = repo.name + ' | Downloading...'
+    const successMessage = repo.name + ' | Success!'
+    const errorMessage = repo.name + ' | Failed!'
+    const donwloadSpinner = createSpinner(downloadMessage)
+    donwloadSpinner.start()
+
+    const writer = fs.createWriteStream(`${dir_downloadFolder}master${index}.zip`);
+  
+    (await fetch(repo.html_url)).body.pipe(writer);
+  
+    return new Promise((resolve, reject) => {
+     writer.on('finish', () => {
+  
+
+              donwloadSpinner.success({ text: successMessage })
+
+                var zip = new admZip(`${dir_downloadFolder}master${index}.zip`)
+                zip.extractAllTo(dir_downloadFolder)
+                try {
+                    fs.unlinkSync(`${dir_downloadFolder}master${index}.zip`)
+                } catch (err) {
+                    console.error(err)
+                }
+
+        resolve
+     });
+     writer.on('error', reject);
+    });
+    
 }
 
-const downloadRepos = async () => {
-    const date = new Date(Date.now())
-    var folderDate =
+const main = async () => {
+    if (!fs.existsSync('./repositories')) {
+        fs.mkdirSync('./repositories')
+    }
+
+    let searchParam = await inputPrams()
+
+    const repositories = await getRepos(searchParam)
+
+        const date = new Date(Date.now())
+
+    const folderDate =
         (`0${date.getDate()}`).slice(-2) + '.'
         + (`0${date.getMonth()}`).slice(-2) + '.'
         + date.getFullYear() + ' ('
@@ -89,39 +106,25 @@ const downloadRepos = async () => {
         + (`0${date.getMinutes()}`).slice(-2) + 'm-'
         + (`0${date.getSeconds()}`).slice(-2) + 's)'
 
-    var dir_downloadFolder = './repositories/' + folderDate
+    const dir_downloadFolder = `./repositories/${folderDate}/`
+
     fs.mkdirSync(dir_downloadFolder)
 
-    const handleDownloads = async (repo) => {
+    const downloads = repositories.map(repo => {
+        return {
+            name: repo.name,
+            html_url: `${repo.html_url}/archive/master.zip`
+        }
+    })
 
-        const downloadMessage = repo.name + ' | Downloading...'
-        const successMessage = repo.name + ' | Success!'
-        const errorMessage = repo.name + ' | Failed!'
-        const donwloadSpinner = createSpinner(downloadMessage)
-        donwloadSpinner.start()
+    console.log(downloads)
 
-        request
-            .get(repo.html_url)
-            .on('error', (error) => {
-                donwloadSpinner.error({ text: errorMessage, mark: '❌' })
-            })
-            .pipe(fs.createWriteStream(dir_downloadFolder + '/master.zip'))
-            .on('finish', () => {
-                donwloadSpinner.success({ text: successMessage })
+    const promises = downloads.map(async (element, index) => await handleDownloads(element, dir_downloadFolder, index))
 
-                var zip = new admZip(dir_downloadFolder + '/master.zip');
-                zip.extractAllTo(dir_downloadFolder)
-                try {
-                    fs.unlinkSync(dir_downloadFolder + '/master.zip')
-                } catch (err) {
-                    console.error(err)
-                }
-            })
-    }
-
-    for await(const element of downloads){
-        handleDownloads(element)
-    }
-    
+    Promise.all(promises)
 }
-await downloadRepos()
+
+
+await main()
+
+
